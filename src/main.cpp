aa5d05e1dd33ddc4b88c554e8830908dfc5ca3f0
lightnap <lightnap1013@gmail.com>
2024-12-15 10:07:27 +0100

    FEATURE(PROJECT): Moved raylib to external. Made basic window opening program and it compiles.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
+#include <raylib/src/raylib.h>
+
 
 int main (void)
 {
 	std::cout << "[LIGHTSAYS ^. .^] HELLO WORLD \n";
 
+
+	InitWindow(800, 450, "my_first_window");
+	SetTargetFPS(60);
+
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("[LIGHTSAYS ^. .^] WELP, I'M A WINDOW NOW", 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+	
+	CloseWindow();
 	return 0;
 }
