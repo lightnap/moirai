2dc95b283008fc0b456cc53e9e52f37c470d1e8c
lightnap <lightnap1013@gmail.com>
2025-01-12 10:25:07 +0100

    FEATURE: Added clean functionality to the do.py script

--- tools/do.py
+++ tools/do.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python3
 # This script is meant for building, dependency handling and cleaning build artifacts.
 # In general it handles all project actions whose commands are long.
 
 import argparse
+import pathlib
+import shutil
 
 
 def configure_argument_parser(parser):
@@ -15,27 +18,60 @@ def configure_argument_parser(parser):
     # TODO: Sone level of verbosity,
 
 
-def check_argument_validity(parser, args):
-    if not args.clean and not args.build:
-        parser.error("No action requested, please select one of the available actions")
+def delete_all_directory_files(dir_path):
+    print(
+        "[DO][CLEAN]: Deleting all files from directory [",
+        dir_path.parent.name + "/" + dir_path.name,
+        "]",
+    )
+
+    for file_path in pathlib.Path.iterdir(dir_path):
+        try:
+            if pathlib.Path.is_file(file_path) or pathlib.Path.is_symlink(file_path):
+                pathlib.Path.unlink(file_path)
+                # print("  Deleting file/symling", file_path) # TODO: Implement verbosity levels.
+            elif pathlib.Path.is_dir(file_path):
+                shutil.rmtree(file_path)
+                # print("  Deleting directory", file_path) # TODO: Implement verborisy levels.
+        except Exception as e:
+            print("[DO][CLEAN]: Failed to delete %s. Reason: %s" % (file_path, e))
 
 
 def perform_clean_action():
-    print("[DO SCRIPT]: Cleaning all previous builds")
-    # TODO: The commands that clean previous builds
+    print("[DO][CLEAN]: Cleaning all previous builds")
+
+    main_project_dir_path = pathlib.Path(__file__).resolve().parents[1]
+    build_dir_path = main_project_dir_path.joinpath("build")
+    bin_dir_path = main_project_dir_path.joinpath("bin")
+    external_dir_path = main_project_dir_path.joinpath("external")
+    raylib_dir_path = external_dir_path.joinpath("raylib")
+
+    delete_all_directory_files(build_dir_path)
+    delete_all_directory_files(raylib_dir_path)
+    delete_all_directory_files(bin_dir_path)
+
+    print("[DO][CLEAN]: Finished!")
 
 
 def perform_build_action():
-    print("[DO SCRIPT]: Building the moirai project")
+    print("[DO][BUILD]: Building the moirai project")
+
+    main_project_dir_path = pathlib.Path(__file__).resolve().parents[1]
+    build_dir_path = main_project_dir_path.joinpath("build")
+    bin_dir_path = main_project_dir_path.joinpath("bin")
+
     # TODO: The commands that build the project
+    print("[DO][BUILD]: Finished!")
 
 
 def main():
-    print("[DO SCRIPT]: Invoked the project manager script")
+    print("[DO]: Invoked the project manager script")
     parser = argparse.ArgumentParser(description="Script to manage the moirai project.")
     configure_argument_parser(parser)
     args = parser.parse_args()
-    check_argument_validity(parser, args)
+
+    if not args.clean and not args.build:
+        parser.error("No action requested, please select one of the available actions")
 
     if args.clean:
         perform_clean_action()
@@ -43,7 +79,7 @@ def main():
     if args.build:
         perform_build_action()
 
-    print("[DO SCRIPT]: Finished!")
+    print("[DO]: Finished!")
 
 
 if __name__ == "__main__":
